d98ef695b34810bad1dc607921dabb837b2b1cee
Vari <tmarksinbox@gmail.com>
2023-01-20 02:11:34 +0000

    feat: create example plugin

---
+++ DamageTrackerExample/Entry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DamageTrackerLib;
+using DamageTrackerLib.DamageInfo;
+using Rage;
+using Rage.Attributes;
+
+[assembly: Plugin("DamageTrackerExample", Description = "A plugin for testing.",
+    Author = "Variapolis",
+    PrefersSingleInstance = true)]
+
+namespace TestCallouts
+{
+    // ReSharper disable once UnusedType.Global
+    public class EntryPoint
+    {
+        private static GameFiber GameFiber;
+        
+        // ReSharper disable once UnusedMember.Global
+        public static void Main()
+        {
+            Game.DisplayNotification("DamageTrackerExample by Variapolis ~g~Successfully Loaded");
+            DamageTrackerService.Start();
+            DamageTrackerService.OnPedTookDamage += PrintDamage;
+            DamageTrackerService.OnPlayerTookDamage += PrintDamage;
+            GameFiber.Hibernate();
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public static void OnUnload(bool Exit)
+        {
+            DamageTrackerService.Stop();
+            Game.DisplayNotification("DamageTrackerExample by Variapolis ~r~ Unloaded");
+        }
+
+        private static void PrintDamage(Ped ped, PedDamageInfo damageInfo) =>
+            Game.DisplayHelp($"{ped.Model.Name} was damaged by a {Enum.GetName(typeof(DamageType), damageInfo.WeaponInfo.Type)}");
+    }
+    
+    
+}
