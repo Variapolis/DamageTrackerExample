fca03682ad1b78a9aa6257ce12e3f63c1da64aba
Vari <tmarksinbox@gmail.com>
2023-02-07 00:35:58 +0000

    feat: add attacker to print function

--- DamageTrackerExample/Entry.cs
+++ DamageTrackerExample/Entry.cs
@@ -15,7 +15,7 @@ namespace TestCallouts
     public class EntryPoint
     {
         private static GameFiber GameFiber;
-        
+
         // ReSharper disable once UnusedMember.Global
         public static void Main()
         {
@@ -33,11 +33,10 @@ namespace TestCallouts
             Game.DisplayNotification("DamageTrackerExample by Variapolis ~r~ Unloaded");
         }
 
-        private static void PrintDamage(Ped ped, PedDamageInfo damageInfo) =>
+        private static void PrintDamage(Ped ped, Ped attackerPed, PedDamageInfo damageInfo) =>
             Game.DisplayHelp($"~w~{ped.Model.Name} (~r~{damageInfo.Damage} Dmg~w~) ({(ped.IsAlive ? "~g~Alive" : "~r~Dead")}~w~)" +
+                             $"\n~r~{attackerPed?.Model.Name ?? "None"}" +
                              $"\n~y~{damageInfo.WeaponInfo.Hash.ToString()} {damageInfo.WeaponInfo.Type.ToString()} {damageInfo.WeaponInfo.Group.ToString()}" +
-                             $"\n~r~{damageInfo.BoneInfo.BoneId.ToString()} { damageInfo.BoneInfo.Limb.ToString()} {damageInfo.BoneInfo.BodyRegion.ToString()}");
+                             $"\n~r~{damageInfo.BoneInfo.BoneId.ToString()} {damageInfo.BoneInfo.Limb.ToString()} {damageInfo.BoneInfo.BodyRegion.ToString()}");
     }
-    
-    
 }
